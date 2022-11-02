71d5ef807ccb35b2b49c89ad7d7150588e257e3b
Ömer YILMAZEL <omeryilmazel@gmail.com>
2022-11-02 23:31:13 +0300

    Update colors.dart

--- lib/src/styles/colors.dart
+++ lib/src/styles/colors.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 class SwColors{
   static Color mainColor = Color(0xFF0067F4);
-  static Color errorColor = Color(0xFFD13E37);
-  static Color warningColor = Color(0xFFF4A362);
+  static Color errorColor = Color(0xFFF40612);
+  static Color warningColor = Color(0xFFFFC600);
 }
