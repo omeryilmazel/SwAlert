6c4da18de7e48488fe592990e675f592167d8083
Ömer YILMAZEL <omeryilmazel@gmail.com>
2022-11-03 23:14:50 +0300

    Update text_style.dart

--- lib/src/styles/text_style.dart
+++ lib/src/styles/text_style.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:sw_alert/src/styles/colors.dart';
 
 class AlertTextStyle{
-  static var title = TextStyle(color: Color(0xFF424242),fontSize: 24,fontWeight: FontWeight.bold);
-  static var content = TextStyle(color: Color(0xFF424242),fontSize: 16);
-  static var buttonText = TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 16);
-  static var contentTitle = TextStyle(color: Color(0xFF424242),fontWeight: FontWeight.bold,fontSize: 16);
+  static var title = TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold);
+  static var content = TextStyle(color: Color(0xA3FFFFFF),fontSize: 14);
+  static var buttonText = TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 14);
+  static var contentTitle = TextStyle(color: Color(0xFFFFFFFF),fontWeight: FontWeight.bold,fontSize: 16);
   static var skipText = TextStyle(color: SwColors.mainColor,fontWeight: FontWeight.w700);
-
 }
